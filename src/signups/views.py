97068fcc4ca6edc7468932ac6a352f4f6217f4e8
annebrogdon <anne.brogdon@gmail.com>
2014-04-14 00:05:44 -0600

    Fixing Nav to Specs and fixing form

--- src/signups/views.py
+++ src/signups/views.py
@@ -30,6 +30,12 @@ def twittersignup(request):
 
 def createtalk(request):
 
+    form = SignUpForm(request.POST or None)
+    
+    if form.is_valid():
+        save_it = form.save(commit=False)
+        save_it.save()
+        
     return render_to_response("createtalk.html",
                               locals(),
                               context_instance=RequestContext(request))
